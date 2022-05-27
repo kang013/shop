6896a5e3378048e776036f9ddf22c4c40491e7bc
hezhenkang <626961483@qq.com>
2022-05-27 16:45:09 +0800

    图片验证码

--- routes/api.php
+++ routes/api.php
@@ -7,15 +7,17 @@ Route::prefix('v1')
     ->namespace('Api')
     ->name('api.v1.')
     ->group(function () {
-    // 短信验证码
-    Route::post('verificationCodes', 'VerificationCodesController@store')
-        ->name('verificationCodes.store');
-    // 用户注册
-    Route::post('users', 'UsersController@store')
-        ->name('users.store');
+        // 短信验证码
+        Route::post('verificationCodes', 'VerificationCodesController@store')
+            ->name('verificationCodes.store');
+        // 用户注册
+        Route::post('users', 'UsersController@store')
+            ->name('users.store');
 
         Route::middleware('throttle:' . config('api.rate_limits.access'))
             ->group(function () {
-
+                // 图片验证码
+                Route::post('captchas', 'CaptchasController@store')
+                    ->name('captchas.store');
             });
-});
+    });
