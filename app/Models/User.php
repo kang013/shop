31c9690f13b233d79cd5a9eb6625170fb21e20ef
hezhenkang <626961483@qq.com>
2022-05-27 17:20:27 +0800

    使用 JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Encore\Admin\Traits\DefaultDatetimeFormat;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable implements MustVerifyEmailContract, JWTSubject
 {
     use DefaultDatetimeFormat;
     use HasFactory, Notifiable;
@@ -73,5 +75,15 @@ class User extends Authenticatable implements MustVerifyEmail
         }
 
         $this->attributes['password'] = $value;
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
