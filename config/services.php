ae7a006c9ae1ca901b1b9b7d7812cff596380076
hezhenkang <626961483@qq.com>
2022-05-27 16:48:06 +0800

    微信登录

--- config/services.php
+++ config/services.php
@@ -29,5 +29,10 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'wechat' => [
+        'client_id' => env('WEIXIN_KEY'),
+        'client_secret' => env('WEIXIN_SECRET'),
+        'redirect' => env('WEIXIN_REDIRECT_URI'),
+    ],
 
 ];
