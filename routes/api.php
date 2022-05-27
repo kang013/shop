28013e5cfbd4e94fa375d85f52327438928df768
hezhenkang <626961483@qq.com>
2022-05-27 16:20:44 +0800

    频率限制

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
+Route::prefix('v1')
+    ->namespace('Api')
+    ->name('api.v1.')
+    ->group(function () {
     // 短信验证码
     Route::post('verificationCodes', 'VerificationCodesController@store')
         ->name('verificationCodes.store');
     // 用户注册
     Route::post('users', 'UsersController@store')
         ->name('users.store');
+
+        Route::middleware('throttle:' . config('api.rate_limits.access'))
+            ->group(function () {
+
+            });
 });
