6726d1b78ca3bcdd923f5e60b7a42231723a4937
hezhenkang <626961483@qq.com>
2022-05-30 15:58:03 +0800

    商品修改

--- app/Http/Controllers/Api/ProductsController.php
+++ app/Http/Controllers/Api/ProductsController.php
@@ -1,38 +1,137 @@
 <?php
 
-
 namespace App\Http\Controllers\Api;
 
-use App\Exceptions\InvalidRequestException;
-use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Exceptions\InvalidRequestException;
+use App\Models\OrderItem;
+use App\Models\Category;
+use Illuminate\Pagination\LengthAwarePaginator;
+use App\SearchBuilders\ProductSearchBuilder;
+use App\Services\ProductService;
 
 class ProductsController extends Controller
 {
     public function index(Request $request)
     {
-        $products = Product::query()->where('on_sale', true)->paginate(5);
+        $page    = $request->input('page', 1);
+        $perPage = 16;
+        // 新建查询构造器对象，设置只搜索上架商品，设置分页
+        $builder = (new ProductSearchBuilder())->onSale()->paginate($perPage, $page);
 
-        return response()->json($products);
+        if ($request->input('category_id') && $category = Category::find($request->input('category_id'))) {
+            // 调用查询构造器的类目筛选
+            $builder->category($category);
+        }
+
+        if ($search = $request->input('search', '')) {
+            $keywords = array_filter(explode(' ', $search));
+            // 调用查询构造器的关键词筛选
+            $builder->keywords($keywords);
+        }
+
+        if ($search || isset($category)) {
+            // 调用查询构造器的分面搜索
+            $builder->aggregateProperties();
+        }
+
+        $propertyFilters = [];
+        if ($filterString = $request->input('filters')) {
+            $filterArray = explode('|', $filterString);
+            foreach ($filterArray as $filter) {
+                list($name, $value) = explode(':', $filter);
+                $propertyFilters[$name] = $value;
+                // 调用查询构造器的属性筛选
+                $builder->propertyFilter($name, $value);
+            }
+        }
+
+        if ($order = $request->input('order', '')) {
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    // 调用查询构造器的排序
+                    $builder->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+
+        // 最后通过 getParams() 方法取回构造好的查询参数
+        $result = app('es')->search($builder->getParams());
+
+        // 通过 collect 函数将返回结果转为集合，并通过集合的 pluck 方法取到返回的商品 ID 数组
+        $productIds = collect($result['hits']['hits'])->pluck('_id')->all();
+        // 通过 whereIn 方法从数据库中读取商品数据
+        $products = Product::query()->byIds($productIds)->get();
+
+        // 返回一个 LengthAwarePaginator 对象
+        $pager = new LengthAwarePaginator($products, $result['hits']['total']['value'], $perPage, $page, [
+            'path' => route('products.index', false), // 手动构建分页的 url
+        ]);
+
+        $properties = [];
+        // 如果返回结果里有 aggregations 字段，说明做了分面搜索
+        if (isset($result['aggregations'])) {
+            $properties = collect($result['aggregations']['properties']['properties']['buckets'])
+                ->map(function ($bucket) {
+                    // 通过 map 方法取出我们需要的字段
+                    return [
+                        'key'    => $bucket['key'],
+                        'values' => collect($bucket['value']['buckets'])->pluck('key')->all(),
+                    ];
+                })
+                ->filter(function ($property) use ($propertyFilters) {
+                    // 过滤掉只剩下一个值 或者 已经在筛选条件里的属性
+                    return count($property['values']) > 1 && !isset($propertyFilters[$property['key']]) ;
+                });
+        }
+
+        return response()->json([
+            'products' => $pager,
+            'filters'  => [
+                'search' => $search,
+                'order'  => $order,
+            ],
+            'category' => $category ?? null,
+            'properties' => $properties,
+            'propertyFilters' => $propertyFilters,
+        ]);
     }
 
-    public function show(Product $product, Request $request)
+    public function show(Product $product, Request $request, ProductService $service)
     {
-        // 判断商品是否已经上架，如果没有上架则抛出异常。
         if (!$product->on_sale) {
             throw new InvalidRequestException('商品未上架');
         }
 
-        return response()->json($product);
+        $favored = false;
+        // 用户未登录时返回的是 null，已登录时返回的是对应的用户对象
+        if($user = $request->user()) {
+            // 从当前用户已收藏的商品中搜索 id 为当前商品 id 的商品
+            // boolval() 函数用于把值转为布尔值
+            $favored = boolval($user->favoriteProducts()->find($product->id));
+        }
+
+        $reviews = OrderItem::query()
+            ->with(['order.user', 'productSku']) // 预先加载关联关系
+            ->where('product_id', $product->id)
+            ->whereNotNull('reviewed_at') // 筛选出已评价的
+            ->orderBy('reviewed_at', 'desc') // 按评价时间倒序
+            ->limit(10) // 取出 10 条
+            ->get();
+
+        $similarProductIds = $service->getSimilarProductIds($product, 4);
+        $similarProducts   = Product::query()->byIds($similarProductIds)->get();
+
+        // 最后别忘了注入到模板中
+        return response()->json([
+            'product' => $product,
+            'favored' => $favored,
+            'reviews' => $reviews,
+            'similar' => $similarProducts,
+        ]);
     }
 
-    /**
-     * 收藏
-     *
-     * @param Product $product
-     * @param Request $request
-     * @return array
-     */
     public function favor(Product $product, Request $request)
     {
         $user = $request->user();
@@ -45,13 +144,6 @@ class ProductsController extends Controller
         return [];
     }
 
-    /**
-     * 取消收藏
-     *
-     * @param Product $product
-     * @param Request $request
-     * @return array
-     */
     public function disfavor(Product $product, Request $request)
     {
         $user = $request->user();
@@ -60,16 +152,10 @@ class ProductsController extends Controller
         return [];
     }
 
-    /**
-     * 收藏列表
-     *
-     * @param Request $request
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function favorites(Request $request)
     {
         $products = $request->user()->favoriteProducts()->paginate(16);
 
-        return response()->json($products);
+        return response()->json(['products' => $products]);
     }
 }
